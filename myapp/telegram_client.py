e03431a471c807268648c5643804a40830c6106a
NickRovneiko <nik.rovneiko@gmail.com>
2024-04-20 23:49:27 +0800

    2

---
+++ myapp/telegram_client.py
@@ -0,0 +1,14 @@
+# myapp/telegram_client.py
+from telethon import TelegramClient
+from django.conf import settings
+
+# Создаем экземпляр клиента Telegram
+telegram_client = TelegramClient('anon_session', '21992840', 'da0843d28758f33ad168037e277f7bb0')
+
+# Функция для запуска клиента, если это еще не было сделано
+async def start_telegram_client():
+    if not telegram_client.is_connected():
+        await telegram_client.connect()
+    if not await telegram_client.is_user_authorized():
+        print("Client is not authorized. Check your settings.")
+        # Тут можно добавить процедуру авторизации, если это необходимо
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, re_path
 from . import views
+from .dialogue_views import show_dialogues, trigger_fetch_dialogues
 
 urlpatterns = [
-    path('', views.show_dialogues, name='home'),  # Сделать страницу диалогов главной страницей приложения
-    path('dialogues/', views.show_dialogues, name='show_dialogues'),
-    path('fetch-dialogues/', views.trigger_fetch_dialogues, name='fetch_dialogues'),
+    path('', show_dialogues, name='home'),  # Сделать страницу диалогов главной страницей приложения
+    path('dialogues/', show_dialogues, name='show_dialogues'),
+    path('fetch-dialogues/', trigger_fetch_dialogues, name='fetch_dialogues'),
     path('search-dialogues/', views.search_dialogues, name='search_dialogues'),
     path('add-dialogue/', views.add_dialogue, name='add_dialogue'),
 ]
