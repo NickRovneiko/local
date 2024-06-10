73f085ddb801fdaa2d6ee1a2683697bb8b6c50dc
NickRovneiko <nik.rovneiko@gmail.com>
2024-06-10 20:07:19 +0800

    поправил время отоборажение и еще что-то

--- myapp/dialogs.py
+++ myapp/dialogs.py
@@ -1,6 +1,5 @@
-# myproject/myapp/dialogs.py
-
 from telethon import TelegramClient
+from datetime import datetime
 
 api_id = '21992840'  # Ваш API ID
 api_hash = 'da0843d28758f33ad168037e277f7bb0'  # Ваш API Hash
@@ -17,12 +16,12 @@ async def fetch_dialogues():
     await client.disconnect()
     return dialogues
 
-async def get_messages_by_dialogue_id(dialogue_id):
+async def get_messages_by_dialogue_id(dialogue_id, last_message_date=None):
     messages_list = []  # Список для хранения данных сообщений
     async with TelegramClient('session_name', api_id, api_hash) as client:
         try:
             entity = await client.get_input_entity(dialogue_id)
-            async for message in client.iter_messages(entity, limit=1000):
+            async for message in client.iter_messages(entity, offset_date=last_message_date, limit=100):
                 # Собираем данные сообщения в словарь
                 message_data = {
                     'message_id': message.id,
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,3 @@
-# myproject/myapp/views.py
-
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.template.loader import render_to_string
@@ -10,13 +8,12 @@ import datetime
 from .dialogs import get_messages_by_dialogue_id, fetch_dialogues  # Импорт fetch_dialogues
 
 from asgiref.sync import sync_to_async
-
+from datetime import datetime
 
 # Функция для вызова снаружи
 @sync_to_async
 def get_dialogues():
     return list(Dialogue.objects.filter(is_added=True))
-
 
 @sync_to_async
 def create_or_update_dialogue(dialogue_data):
@@ -25,12 +22,10 @@ def create_or_update_dialogue(dialogue_data):
         defaults={'name': dialogue_data['name']}
     )
 
-
 async def save_dialogues_to_db():
     dialogues = await fetch_dialogues()
     for dialogue_data in dialogues:
         await create_or_update_dialogue(dialogue_data)
-
 
 @sync_to_async
 def save_message(data, dialogue):
@@ -47,21 +42,28 @@ def save_message(data, dialogue):
         }
     )
 
+@sync_to_async
+def get_last_message_date(dialogue):
+    last_message = Message.objects.filter(dialogue=dialogue).order_by('-date').first()
+    if last_message:
+        return last_message.date
+    return None
 
 async def fetch_and_save_messages(request):
     dialogues = await get_dialogues()
     for dialogue in dialogues:
-        messages_data = await get_messages_by_dialogue_id(dialogue.dialogue_id)
+        last_message_date = await get_last_message_date(dialogue)
+        if last_message_date:
+            last_message_date = last_message_date.replace(tzinfo=None)  # Убираем информацию о временной зоне
+        messages_data = await get_messages_by_dialogue_id(dialogue.dialogue_id, last_message_date)
         for data in messages_data:
             await save_message(data, dialogue)
     return JsonResponse({'status': 'success', 'message': 'Messages have been updated successfully.'})
-
 
 @csrf_exempt
 async def load_dialogues(request):
     await save_dialogues_to_db()
     return JsonResponse({'status': 'success', 'message': 'Dialogues have been loaded successfully.'})
-
 
 def show_dialogues(request):
     if request.method == "POST":
@@ -77,13 +79,11 @@ def show_dialogues(request):
         'added_dialogues': added_dialogues
     })
 
-
 def search_dialogues(request):
     search_query = request.GET.get('search', '').strip()
     dialogues = Dialogue.objects.filter(name__icontains=search_query)[:10]  # Ограничиваем количество результатов
     html = render_to_string('search_results.html', {'dialogues': dialogues})
     return HttpResponse(html)
-
 
 @csrf_exempt
 def add_dialogue(request):
@@ -93,13 +93,11 @@ def add_dialogue(request):
     dialogue.save()
     return JsonResponse({'status': 'success', 'name': dialogue.name})
 
-
 def get_recent_messages(request):
     dialogue_id = request.GET.get('dialogue_id')
     messages = Message.objects.filter(dialogue__dialogue_id=dialogue_id).order_by('-date')[:20]
     messages_html = render_to_string('recent_messages.html', {'messages': messages})
     return HttpResponse(messages_html)
-
 
 @csrf_exempt
 def delete_dialogue(request):
@@ -112,8 +110,6 @@ def delete_dialogue(request):
         else:
             return JsonResponse({'status': 'error', 'message': 'Dialogue not found'})
     return JsonResponse({'status': 'error', 'message': 'Invalid request'})
-
-from django.db.models import Q
 
 from django.db.models import Q
 from django.utils.html import escape
